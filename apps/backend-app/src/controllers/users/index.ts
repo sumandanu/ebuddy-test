2382a7eca5c6d263e79cfbbc793e7c3f7e0ffef8
olan sumandanu <olan.sumandanu@gmail.com>
2025-03-17 08:16:32 +0700

    fix(backend-app): clean code

--- apps/backend-app/src/controllers/users/index.ts
+++
@@ -1,5 +0,0 @@
-import { listUsers } from "../../services/users";
-
-import { buildGetUsers } from "./fetch-user-data";
-
-export const getUsers = buildGetUsers({ listUsers });
--- apps/backend-app/src/routes/v1/users.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getUsers } from "../../controllers/users";
-import { buildExpressCallback } from "../../helpers/express-callback";
-
-const router: Router = Router();
-
-router.get("/", buildExpressCallback(getUsers));
-
-export default router;
